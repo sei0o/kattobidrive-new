ecabe441711d820666e6a3020066460660d0aca4
Seioo Inoue <sei0o@live.jp>
2021-11-21 19:23:23 +0900

    Use axios instead of node-fetch

--- netlify/functions/call_api.js
+++ netlify/functions/call_api.js
@@ -1,19 +1,17 @@
-exports.handler = async (event, _context) => {
-  const fetch = (...args) =>
-    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const axios = require("axios");
 
+exports.handler = async (event, _context) => {
   const ACCESS_TOKEN = "PUT_YOUR_TOKEN_HERE";
   const ENDPOINT = "https://api.chunirec.net/2.0/records/showall.json";
 
   const userName = JSON.parse(event.body)["user_name"];
 
-  const resp = await fetch(
+  const resp = await axios.get(
     ENDPOINT + `?user_name=${userName}&region=jp2&token=${ACCESS_TOKEN}`
   );
-  const json = await resp.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(json),
+    body: resp.data,
   };
 };
